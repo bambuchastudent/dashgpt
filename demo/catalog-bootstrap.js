709f10bab1fed2036226b23ef7352b3e85eae5b4
Dmitrii Kashirin <bambuchastudent@gmail.com>
2026-08-10 23:28:02 +0530

    Normalize mobile ChatGPT share links before onboarding

--- demo/catalog-bootstrap.js
+++ demo/catalog-bootstrap.js
@@ -77,5 +77,6 @@ if (!document.querySelector('link[data-dashgpt-onboarding]')) {
 }
 
 await import("./app.js");
+await import("./share-link-compat.js");
 await import("./public-onboarding.js");
 await import("./product-board-discovery.js");
