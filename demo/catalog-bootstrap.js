0169b32125a1fd397ae15cb6d6176dbe173a92b0
Dmitrii Kashirin <bambuchastudent@gmail.com>
2026-08-14 15:01:35 +0530

    F34 load Google account entry

--- demo/catalog-bootstrap.js
+++ demo/catalog-bootstrap.js
@@ -105,6 +105,7 @@ if (feature20PersonalEntry) {
 
 await import("./app.js");
 await import("./google-drive-sync.js");
+await import("./google-account-entry.js");
 await import("./share-link-compat.js");
 await import("./public-onboarding.js");
 await import("./anonymous-share-onboarding.js");
